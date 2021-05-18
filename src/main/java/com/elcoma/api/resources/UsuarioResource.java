3dda7d029da8c9deb8a0c5ac5678a9c4aac5677d
Antony Maia <55353029+antonymaia@users.noreply.github.com>
2021-05-17 23:03:26 -0300

    Update UsuarioResource.java

--- src/main/java/com/elcoma/api/resources/UsuarioResource.java
+++ src/main/java/com/elcoma/api/resources/UsuarioResource.java
@@ -19,6 +19,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/usuarios")
 public class UsuarioResource {
