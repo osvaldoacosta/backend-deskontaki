b4e94818e2ebd26c0f99dc14027b7288384d0eb5
Antony Maia <antonymaiati@gmail.com>
2021-06-08 19:30:22 -0300

    Updating insert NotaFiscal

--- src/main/java/com/elcoma/api/App.java
+++ src/main/java/com/elcoma/api/App.java
@@ -1,4 +1,5 @@
 package com.elcoma.api;
+
 import com.elcoma.api.domain.Cliente;
 import com.elcoma.api.domain.Usuario;
 import com.elcoma.api.repositories.ClienteRepository;
@@ -8,10 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class App  {
+public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
 }
--- src/main/java/com/elcoma/api/resources/NotaFiscalResource.java
+++ src/main/java/com/elcoma/api/resources/NotaFiscalResource.java
@@ -7,12 +7,14 @@ import com.elcoma.api.services.NotaFiscalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/notafiscal")
 public class NotaFiscalResource {
@@ -20,49 +22,48 @@ public class NotaFiscalResource {
     @Autowired
     private NotaFiscalService service;
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<LojaDTO> insert(@Valid @RequestBody NotaFiscal notaFiscal) throws IOException {
+        LojaDTO lojaDTO =  service.register(notaFiscal.getUrl());
+        return ResponseEntity.ok().body(lojaDTO);
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<NotaFiscal> findById(@PathVariable Integer id){
+    public ResponseEntity<NotaFiscal> findById(@PathVariable Integer id) {
         NotaFiscal notaFiscal = service.findById(id);
-        return  ResponseEntity.ok().body(notaFiscal);
+        return ResponseEntity.ok().body(notaFiscal);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Void> update(@Valid @RequestBody NotaFiscal notaFiscal, @PathVariable Integer id){
+    public ResponseEntity<Void> update(@Valid @RequestBody NotaFiscal notaFiscal, @PathVariable Integer id) {
         notaFiscal.setId(id);
         notaFiscal = service.update(notaFiscal);
         return ResponseEntity.noContent().build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> delete(@PathVariable Integer id){
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<NotaFiscal>> findAll(){
+    public ResponseEntity<List<NotaFiscal>> findAll() {
         List<NotaFiscal> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Object> register(@Valid @RequestBody NotaFiscal notaFiscal) throws IOException, ParseException {
-        LojaDTO lojaDTO = (LojaDTO) service.register(notaFiscal.getUrl());
-        return ResponseEntity.ok().body(lojaDTO.getNome());
-    }
-
-    @RequestMapping(value ="/year/{year}", method = RequestMethod.GET)
-    public ResponseEntity<List<NotaFiscalDTO>> findByYear(@PathVariable String year){
+    @RequestMapping(value = "/year/{year}", method = RequestMethod.GET)
+    public ResponseEntity<List<NotaFiscalDTO>> findByYear(@PathVariable String year) {
         return ResponseEntity.ok().body(service.findByYear(year));
     }
 
-   @RequestMapping(value ="/totalnotas/{idUsuario}/{dataCadastro}", method = RequestMethod.GET)
+    @RequestMapping(value = "/totalnotas/{idUsuario}/{dataCadastro}", method = RequestMethod.GET)
     public ResponseEntity<List<NotaFiscal>> findAllByUsuarioAndDataCadastro(
-                                                                @PathVariable Integer idUsuario,
-                                                                @PathVariable String dataCadastro
-   ) throws ParseException {
+            @PathVariable Integer idUsuario,
+            @PathVariable String dataCadastro
+    ) throws ParseException {
         List<NotaFiscal> notaFiscalList = service.findAllByUsuarioAndDataCadastro(idUsuario, dataCadastro.trim());
         return ResponseEntity.ok().body(notaFiscalList);
     }
-
 }
